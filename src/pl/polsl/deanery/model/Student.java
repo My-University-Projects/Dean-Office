3374bd4cd1986582aab4a966176e88840cda989d
Tomasz Krypczyk <tomakry729@student.polsl.pl>
2022-04-03 12:03:27 +0200

    Clean repo

---
+++ src/pl/polsl/deanery/model/Student.java
@@ -0,0 +1,55 @@
+package pl.polsl.deanery.model;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Student {
+	
+	@Id
+	@GeneratedValue
+	private Integer id;
+	private String name;
+	private String surname;
+	private Date birthDate;
+	@OneToMany(mappedBy = "student")
+	private List<Grade> grades = new ArrayList<Grade>();
+	
+	public List<Grade> getGrades() {
+		return grades;
+	}
+	public void setGrades(List<Grade> grades) {
+		this.grades = grades;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	public Date getBirthDate() {
+		return birthDate;
+	}
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}	
+
+}
